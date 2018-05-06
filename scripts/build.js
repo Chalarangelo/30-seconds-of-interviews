9a1b2409f1c456a749bbb56c13ad978c344cbe8b
Stefan Feješ <ns.fejes.stefan@gmail.com>
2018-05-06 16:43:30 +0200

    update build.js to load static parts

--- scripts/build.js
+++ scripts/build.js
@@ -10,6 +10,17 @@ const QUESTIONS_PATH = './questions';
 const STATIC_PARTS_PATH = './static-parts';
 
 let questions = {};
+let start = '';
+let end = '';
+let output = '';
 
-console.log(util.readQuestions(QUESTIONS_PATH));
+questions = util.readQuestions(QUESTIONS_PATH));
+
+try {
+  startPart = fs.readFileSync(path.join(STATIC_PARTS_PATH, 'README-start.md'), 'utf8');
+  endPart = fs.readFileSync(path.join(STATIC_PARTS_PATH, 'README-end.md'), 'utf8');
+} catch (err) {
+  console.log(`${chalk.red('ERROR!')} During static part loading: ${err}`);
+  process.exit(1);
+}
 
