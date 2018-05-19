debc0e4f77f9055c970fee2864086c2aff67e773
atomiks <cc.glows@gmail.com>
2018-05-19 18:59:08 +1000

    add arrow

--- website/js/components/Icon.js
+++
@@ -1,8 +0,0 @@
-import { h } from "hyperapp"
-import feather from "feather-icons"
-
-const init = () => {
-  feather.replace()
-}
-
-export default ({ icon }) => <i data-feather="icon" oncreate={init} />
