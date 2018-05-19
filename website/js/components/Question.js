b49ce8ddb3a70d5cc5c780355b2eb3b8e9c91318
atomiks <cc.glows@gmail.com>
2018-05-19 22:24:25 +1000

    add filter

--- website/js/components/Question.js
+++ website/js/components/Question.js
@@ -7,13 +7,13 @@ export default ({ name, question, answer, isOpen }) => (state, actions) => (
   <div class="Question">
     <div class="Question__heading" innerHTML={marked(`## ${question}`)} />
     <button
-      class="Question__button"
+      class="btn Question__button"
       onclick={event => actions.onAnswerButtonClick({ event, name, isOpen })}
     >
       {isOpen ? "Hide answer" : "Show answer"}
       <span
-        class={cc("Question__button-icon", { "is-rotated": isOpen })}
-        innerHTML={feather.icons["arrow-down-circle"].toSvg()}
+        class={cc("btn__icon", { "is-rotated": isOpen })}
+        innerHTML={feather.icons["chevron-down"].toSvg()}
       />
     </button>
     <div
--- website/js/view.js
+++ website/js/view.js
@@ -1,10 +1,12 @@
 import { h } from "hyperapp"
 import Header from "./components/Header"
+import Filter from "./components/Filter"
 import Questions from "./components/Questions"
 
 export default (state, actions) => (
   <div>
     <Header />
+    <Filter />
     <Questions />
   </div>
 )
