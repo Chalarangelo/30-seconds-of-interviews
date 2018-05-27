e603627d25d1d3457a03f93ae02474b2e2c99bb8
Felix Wu <mail@flxwu.com>
2018-05-27 13:13:56 +0200

    [FEATURE] Sorting and Sort Button, outsource all key strings (#33)
    * outsource key strings
    
    * make container relative
    
    * import sortbutton scss
    
    * sort button scss
    
    * add sortby action
    
    * add SortButton component
    
    * use nextValInObj util to get next sortby string
    
    * Add function to return next value in object (sorting)
    
    * adapt to new strings constants names
    
    * adapt to TAG_NAMES
    
    * fix button tooltip
    
    * add sortComparison depending on current sortBy condition

--- website/js/actions.js
+++ website/js/actions.js
@@ -1,15 +1,18 @@
-import Prism from "prismjs"
-import { scrollToTop } from "./utils"
+import Prism from "prismjs";
+import { scrollToTop, SORTBY_STRINGS, nextValInObj } from "./utils";
 
 export default {
-  scrollBackToTop: scrollToTop,
-  onAnswerButtonClick: ({ name, isOpen }) => state => ({
-    questions: state.questions.map(
-      q => (q.name === name ? { ...q, isOpen: !isOpen } : q)
-    )
-  }),
-  setFilter: value => {
-    requestAnimationFrame(() => setTimeout(Prism.highlightAll))
-    return { filter: value }
-  }
-}
+	scrollBackToTop: scrollToTop,
+	onAnswerButtonClick: ({ name, isOpen }) => state => ({
+		questions: state.questions.map(
+			q => (q.name === name ? { ...q, isOpen: !isOpen } : q)
+		)
+	}),
+	setFilter: value => {
+		requestAnimationFrame(() => setTimeout(Prism.highlightAll));
+		return { filter: value };
+	},
+	setSortBy: value => state => {
+		return { sortBy: nextValInObj(SORTBY_STRINGS, state.sortBy) };
+	}
+};
