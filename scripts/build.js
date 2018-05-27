12993030f1056e731839c9549776933d63405878
Felix Wu <mail@flxwu.com>
2018-05-27 16:10:23 +0200

    complete builder rewrite: parse questions.json instead of the questions folder

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,9 @@
 const fs = require("fs-extra")
 const path = require("path")
 const chalk = require("chalk")
+const format = require("prettier-eslint")
 const util = require("./util.js")
+const questions = require("../data/questions.json")
 const { QUESTIONS_PATH, TAG_NAMES } = util
 
 const STATIC_PARTS_PATH = "./static-parts"
@@ -30,87 +32,97 @@ const formatOptions = code => {
 }
 
 let tagOrder = ["javascript", "css", "html"]
-let questions = {}
 let questionTags = {}
 let startPart = ""
 let endPart = ""
 let output = ""
 
+// methods for converting to markdown
+const hX = (n, text) => `\n${"#".repeat(n)} ${text}`
+
+const detailsTOC = (title, questionsArray) => {
+	let list = questionsArray
+		.map(
+			question =>
+				`* [${question.question}](#${util.toKebabCase(question.question)})`
+		)
+		.join("\n")
+	return `\n\n<details>\n<summary>${title}</summary>\n\n${list}\n</details>\n\n`
+}
+const detailsQuestion = (title, question) => {
+	let answer = question.answer
+	// add Good to Hear
+	answer += `\n\n${hX(4, "Good to hear")}\n\n`
+	answer += `\n${question.goodToHear.map(s => `* ${s}`).join("\n")}`
+	// add Additional links
+	answer += `\n\n${hX(5, "Additional links")}\n\n`
+	answer += `\n${question.links.map(link => `* ${link}`).join("\n")}`
+	return `\n\n<details>\n<summary>${title}</summary>\n${answer}\n</details>\n\n`
+}
+
 console.time("Builder")
 
-questions = util.readQuestions(QUESTIONS_PATH)
+// questions = util.readQuestions(QUESTIONS_PATH)
 
 try {
-  startPart = fs.readFileSync(
-    path.join(STATIC_PARTS_PATH, "README-start.md"),
-    "utf8"
-  )
-  endPart = fs.readFileSync(
-    path.join(STATIC_PARTS_PATH, "README-end.md"),
-    "utf8"
-  )
+	startPart = fs.readFileSync(
+		path.join(STATIC_PARTS_PATH, "README-start.md"),
+		"utf8"
+	)
+	endPart = fs.readFileSync(
+		path.join(STATIC_PARTS_PATH, "README-end.md"),
+		"utf8"
+	)
 } catch (err) {
-  console.log(`${chalk.red("ERROR!")} During static part loading: ${err}`)
-  process.exit(1)
+	console.log(`${chalk.red("ERROR!")} During static part loading: ${err}`)
+	process.exit(1)
 }
 
 try {
-  // add static part for start
-  output += `${startPart + "\n"}`
+	// add static part for start
+	output += `${startPart + "\n"}`
 
-  // add table of contents
-  for (let questionKey in questions) {
-    let tags = /tags:\s+\((.*)\)/g
-      .exec(questions[questionKey])[1]
-      .split(",")
-      .map(v => v.trim())
-    questionTags[questionKey] = tags
-    tags.forEach(v => (tagOrder.includes(v) ? "" : tagOrder.push(v)))
-  }
+	let questionsInTag = {}
 
-  for (tag of tagOrder) {
-    output += `\n### ${
-      TAG_NAMES[tag]
-    } \n\n<details>\n<summary>View contents</summary>\n\n`
-    let taggedSnippets = Object.keys(questionTags).filter(
-      v => questionTags[v][0] === tag
-    )
-    for (let taggedSnippet of taggedSnippets) {
-      let title = /###.+/g.exec(questions[taggedSnippet])[0]
-      output += `* [${title.replace("###", "")}](#${util.toKebabCase(
-        title.toLowerCase()
-      )}) \n`
-    }
-    output += `</details>\n`
-  }
-  output += "\n---\n"
+	// put questions into respective tag-keyed arrays
+	questions.forEach(question => {
+		question.tags.forEach(tag => {
+			if (Object.keys(questionsInTag).includes(tag)) {
+				questionsInTag[tag].push(question)
+			} else {
+				let newArr = []
+				newArr.push(question)
+				questionsInTag[tag] = newArr
+			}
+		})
+	})
 
-  for (tag of tagOrder) {
-    output += `\n## ${TAG_NAMES[tag]}\n\n`
-    let taggedSnippets = Object.keys(questionTags).filter(
-      v => questionTags[v][0] === tag
-    )
-    for (let taggedSnippet of taggedSnippets) {
-      output += `${questions[taggedSnippet].replace(
-        /####\s+Answer/g,
-        "#### Answer\n\n<details>\n<summary>View answer</summary>"
-      )}</details> \n\n<br>[⬆ Back to top](#table-of-contents)\n\n`
-    }
-    output += "\n---\n"
-  }
+	// write Table of Contents
+	Object.keys(questionsInTag).forEach(tagKey => {
+		taggedQuestions = questionsInTag[tagKey]
+		output += hX(3, TAG_NAMES[tagKey])
+		output += detailsTOC("View content", taggedQuestions)
+	})
 
-  // loop over questions and add them to output
-  // for (let questionKey in questions) {
-  //   output += `${questions[questionKey]}`;
-  // }
+	// delimeter after TOC
+	output += "\n---\n"
 
-  // add static part for end
-  output += `\n${endPart + "\n"}`
+	// write actual questions
+	Object.keys(questionsInTag).forEach(tagKey => {
+		taggedQuestions = questionsInTag[tagKey]
+		taggedQuestions.forEach(question => {
+			output += detailsQuestion("View answer", question)
+			output += `\n\n<br>[⬆ Back to top](#table-of-contents)\n\n`
+		})
+	})
 
-  fs.writeFileSync("README.md", output)
+	// add static part for end
+	output += `\n${endPart + "\n"}`
+
+	fs.writeFileSync("README.md", output)
 } catch (err) {
-  console.log(`${chalk.red("ERROR!")} During README generation: ${err}`)
-  process.exit(1)
+	console.log(`${chalk.red("ERROR!")} During README generation: ${err}`)
+	process.exit(1)
 }
 
 console.log(`${chalk.green("SUCCESS!")} README file generated!`)
