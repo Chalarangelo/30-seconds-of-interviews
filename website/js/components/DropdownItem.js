1a588f4b79a6015bdc2ebc2fdb94aa632e8008ad
atomiks <cc.glows@gmail.com>
2018-05-29 21:01:48 +1000

    active state color

--- website/js/components/DropdownItem.js
+++ website/js/components/DropdownItem.js
@@ -1,15 +1,26 @@
 import { h } from "hyperapp"
-import {cc} from "../utils"
+import { cc, EXPERTISE_STRINGS } from "../utils"
 import Icon from "./Icon"
 
-export default ({ name, icon, dropdown }) => (state, actions) => (
-  <button
-    class="DropdownItem"
-    onclick={() =>
-      actions.filter.set({ dropdown: dropdown.toLowerCase(), name })
-    }
-  >
-    <Icon name={icon} class={`DropdownItem__icon is-${name.toLowerCase()}`} />
-    <span>{name}</span>
-  </button>
-)
+export default ({ name, icon, dropdown }) => (state, actions) => {
+  const dropdownIndex = EXPERTISE_STRINGS.indexOf(name.toLowerCase())
+
+  return (
+    <button
+      class={cc(`DropdownItem is-${name.toLowerCase()}`, {
+        "is-active": state.filter[dropdown.toLowerCase()] === name
+      })}
+      onclick={() =>
+        actions.filter.set({ dropdown: dropdown.toLowerCase(), name })
+      }
+    >
+      {[...Array(dropdownIndex === -1 ? 1 : dropdownIndex + 1)].map(() => (
+        <Icon
+          name={icon}
+          class={`DropdownItem__icon is-${name.toLowerCase()}`}
+        />
+      ))}
+      <span class="DropdownItem__text">{name}</span>
+    </button>
+  )
+}
