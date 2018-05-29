6dd799566e688350d46727fa0107a533a1d1bdb9
Felix Wu <mail@flxwu.com>
2018-05-29 22:32:58 +0200

    readd sortby

--- website/js/components/Filter.js
+++ website/js/components/Filter.js
@@ -1,10 +1,12 @@
 import { h } from "hyperapp"
 import Dropdown from "./Dropdown"
+import SortButton from "./SortButton"
 
 export default () => state => (
   <div class="Filter">
     <div class="container">
       <Dropdown {...state.filter.dropdown} />
+      <SortButton />
     </div>
   </div>
 )
