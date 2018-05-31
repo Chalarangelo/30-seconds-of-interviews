8f60f3e1fd17be1260ae470951ed4a9267aa7c05
Felix Wu <mail@felixwu.dev>
2018-05-31 15:16:32 +0200

    add a force build commit message option

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,4 +7,14 @@ test $TRAVIS_EVENT_TYPE = cron \
           && cd docs/ && ./../.travis/write_cname.sh \
           && cd ../
 
+if [[ $TRAVIS_COMMIT_MESSAGE == *"--force-build"* ]];  
+  then echo -e "\e[95mFORCE-BUILD: Building website." \
+          && rm -rf docs \
+          && yarn build \
+          && touch docs/.nojekyll \
+          && chmod +x .travis/write_cname.sh \
+          && cd docs/ && ./../.travis/write_cname.sh \
+          && cd ../;
+fi
+
 exit 0
