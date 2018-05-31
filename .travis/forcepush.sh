22ddb6f600c1f72611dede314caeeb2a75236208
Felix Wu <mail@felixwu.dev>
2018-05-31 15:29:09 +0200

    fixing the push force --force-build

---
+++ .travis/forcepush.sh
@@ -0,0 +1,5 @@
+if [[ $TRAVIS_COMMIT_MESSAGE == *"--force-build"* ]]; 
+  then echo -e "\e[95mFORCE-DEPLOY: Deploying to Repository" \ 
+    && chmod +x .travis/push.sh \ 
+    && ./.travis/push.sh; 
+fi
