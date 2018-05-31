bab6f962205e9fa874715343f093e08bf724f3ad
30secondsofcode <30secondsofcode@gmail.com>
2018-05-31 13:33:05 +0000

    Travis build: 405

--- website/js/components/Footer.js
+++ website/js/components/Footer.js
@@ -1,9 +1,9 @@
 import { h } from "hyperapp"
 
 export default () => (
-    <div>
-        <footer class="Footer">
-            <p>Made with ❤️ using <a href="https://hyperapp.js.org/">Hyperapp</a></p>
-        </footer>
-    </div>
+  <div>
+    <footer class="Footer">
+      <p>Made with ❤️ using <a href="https://hyperapp.js.org/">Hyperapp</a></p>
+    </footer>
+  </div>
 )
