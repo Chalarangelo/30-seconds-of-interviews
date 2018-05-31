baec25dc94dc2020d0f5b260ea818c472f9ea38c
Felix Wu <mail@felixwu.dev>
2018-05-31 15:31:42 +0200

    syntax err --force-build

--- .travis/forcepush.sh
+++ .travis/forcepush.sh
@@ -1,5 +1,3 @@
 if [[ $TRAVIS_COMMIT_MESSAGE == *"--force-build"* ]]; 
-  then echo -e "\e[95mFORCE-DEPLOY: Deploying to Repository" \ 
-    && chmod +x .travis/push.sh \ 
-    && ./.travis/push.sh; 
+  then echo -e "\e[95mFORCE-DEPLOY: Deploying to Repository" && chmod +x .travis/push.sh && ./.travis/push.sh; 
 fi
