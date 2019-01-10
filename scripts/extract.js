72356014ab93cf6bcb33620e950a78b62bf64f50
Chris Davis <github@vu6.me>
2019-01-10 00:16:27 -0600

    fixed missing heading bug; no have to make subheadings work again

--- scripts/extract.js
+++ scripts/extract.js
@@ -6,42 +6,41 @@ const { attempt, readQuestions, getCodeBlocks } = require("./util")
 
 console.time("Extractor")
 
+const getSection = (searchString, contents) => {
+  const searchIndex = contents.indexOf(searchString)
+  if (searchIndex < 0) return ""
+  const startIndex = searchIndex + searchString.length + 1
+  const stopIndex = contents.indexOf("\n#", startIndex)
+  const endIndex = stopIndex === -1 ? undefined : stopIndex
+
+  return contents.slice(startIndex, endIndex).trim()
+}
+
 attempt("questions.json generation", () => {
   const output = Object.entries(readQuestions()).map(([ name, contents ]) => {
-    const question = contents
-      .slice(0 + 4, contents.indexOf("#### Answer"))
-      .trim()
-    const answer = contents
-      .slice(
-        contents.indexOf("#### Answer") + 12,
-        contents.indexOf("#### Good to hear")
-      )
-      .trim()
+    const question = getSection("", contents.slice(3))
+    const answer = getSection("#### Answer", contents)
 
+    const goodToHear = getSection("#### Good to hear", contents)
+      .split("\n")
+      .map(v => v.replace(/[*-] /g, ""))
+      .filter(v => v.trim() !== "")
+
+    const links = getSection("##### Additional links", contents)
+      .split("\n")
+      .filter(v =>
+        /(\/\*[\w\'\s\r\n\*]*\*\/)|(\/\/[\w\s\']*)|(\<![\-\-\s\w\>\/]*\>)/.test(
+          v
+        )
+      )
+      .map(v => v.replace(/[*-] /g, ""))
+      .filter(v => v.trim() !== "" && !v.includes("tags"))
     return {
       name,
       question,
       answer,
-      goodToHear: contents
-        .slice(
-          contents.indexOf("#### Good to hear") + 18,
-          contents.indexOf("##### Additional links")
-        )
-        .trim()
-        .split("\n")
-        .map(v => v.replace(/[*-] /g, ""))
-        .filter(v => v.trim() !== ""),
-      links: contents
-        .slice(contents.indexOf("##### Additional links") + 23)
-        .trim()
-        .split("\n")
-        .filter(v =>
-          /(\/\*[\w\'\s\r\n\*]*\*\/)|(\/\/[\w\s\']*)|(\<![\-\-\s\w\>\/]*\>)/.test(
-            v
-          )
-        )
-        .map(v => v.replace(/[*-] /g, ""))
-        .filter(v => v.trim() !== "" && !v.includes("tags")),
+      goodToHear,
+      links,
       tags: (contents.match(/<!--\s*tags:\s*\((.+)\)/) || [])[1].split(","),
       expertise: parseInt(
         (contents.match(/<!--\s*expertise:\s*\((.+)\)/) || [])[1],
