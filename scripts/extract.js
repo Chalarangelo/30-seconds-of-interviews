5144965c003df13f93ec0863b6ca08a41728ba57
Chris Davis <github@vu6.me>
2019-01-10 01:05:46 -0600

    cleanup code and check in corrected questions.json

--- scripts/extract.js
+++ scripts/extract.js
@@ -2,67 +2,22 @@
 const fs = require("fs-extra")
 const path = require("path")
 const chalk = require("chalk")
-const { attempt, readQuestions, getCodeBlocks } = require("./util")
+const {
+  attempt,
+  readQuestions,
+  getCodeBlocks,
+  getSection,
+  getFirstSection
+} = require("./util")
 
 console.time("Extractor")
 
-let outName = null
-
-const getSection = (searchString, contents, excludeSubsections) => {
-
-  let log
-  if (outName === "alt-attribute.md" && searchString === "#### Answer") log = (...args) => console.log(...args)
-  else log = () => {}
-
-  log("BEGIN")
-
-  log(searchString)
-
-  const searchIndex = contents.indexOf(searchString)
-  if (searchIndex < 0) return ""
-
-  log(searchIndex)
-
-  let endSearch = "\\n#"
-
-  if(!excludeSubsections){
-    let i
-    for (i = 0; searchString[i] === "#" && i < searchString.length; i++);
-    log(i)
-
-    if (i > 0) {
-      endSearch += `{${i - 1},${i}}[^#]`
-    }
-    
-  }
-  log(endSearch, endSearch.length)
-
-  const endRegex = new RegExp(endSearch)
-  log(endRegex)
-
-  const startIndex = searchIndex + searchString.length + 1
-  log(startIndex)
-  const stopIndex = contents.slice(startIndex).search(endRegex)
-  log(stopIndex)
-  const endIndex = stopIndex === -1 ? undefined : stopIndex + startIndex
-  log(endIndex)
-
-  log("CONTENTS")
-  log(contents)
-  log("/CONTENTS")
-
-  const result = contents.slice(startIndex, endIndex).trim()
-  log(result)
-  return result
-}
-
 attempt("questions.json generation", () => {
   const output = Object.entries(readQuestions()).map(([ name, contents ]) => {
-    outName = name
-    const question = getSection("", contents.slice(3))
+    const question = getFirstSection(contents)
     const answer = getSection("#### Answer", contents)
 
-    const goodToHear = getSection("#### Good to hear", contents, true)
+    const goodToHear = getSection("#### Good to hear", contents, false)
       .split("\n")
       .map(v => v.replace(/[*-] /g, ""))
       .filter(v => v.trim() !== "")
@@ -76,6 +31,7 @@ attempt("questions.json generation", () => {
       )
       .map(v => v.replace(/[*-] /g, ""))
       .filter(v => v.trim() !== "" && !v.includes("tags"))
+
     return {
       name,
       question,
